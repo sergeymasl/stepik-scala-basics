51933237710075923802191e217e8826f1c16314
sergeymasl <serezha_maslov@mail.ru>
2022-09-23 17:25:09 +0300

    2022-09-23

--- src/lectures/week1basics/StringOperations.scala
+++ src/lectures/week1basics/StringOperations.scala
@@ -1,6 +1,7 @@
 package lectures.week1basics
 
 import scala.+:
+import scala.io.StdIn
 
 object StringOperations extends App {
 
@@ -13,10 +14,10 @@ object StringOperations extends App {
 
   // методы
 
-  /*
+
   println(aString.length) // длинна строки // выведет 13
   println(aString.charAt(1)) // выводит символ с индексом в скобках // индексация начинается с 0
-  println(aString.substring(0, 2)) // срез строки // выведет He
+  println(aString.substring(0, 2)) // срез строки // выведет He // символ с вторым индексом не выводится
   println(aString.split(" ").toList) // деление по разделителю // метод toList преобразует объект Java в спикок
   println(aString.startsWith("He")) // проверяет начинается ли строка с символов в скобках // выведет True
   println(aString.replace("!", ".")) // замена символов
@@ -24,26 +25,38 @@ object StringOperations extends App {
   println(aString.toUpperCase) // в верхний регистр
   println("abcd".reverse) // переворачивает строку // выведет dcba
   println("abcd".take(3)) // обирает n количество первых символов
-  */
 
+  //============================================input==================================================
+  // https://scala-lang.org/api/current/scala/io/StdIn%24.html
+  val test_input = scala.io.StdIn.readLine()
+  // readBoolean() : Boolean
+  // readByte() : Byte
+  // readChar() : Char
+  // readDouble() : Double
+  // readFloat() : Float
+  // readInt() : Int
+  // readLine() : String
+  // readLong() : Long
+  // readShort() : Short
+  // something other ...
 
   //===========================добавление char в начало или конец string===============================
 
-  /*
+
   println('1' +: "42" :+ '3') // выводит 1423
   println('a' +: "bc" :+ 'd') // abcd
   println("a" ++: "bc" :++ "d") // abcd
   println("a" ++ "bc" + "d")
-  */
 
   // если Char добавляется в начало String, то используется +:
   // если Char добавляется в конец String, то используется :+
-  // двоеточие в данном случае означает где находится String
+  // ТО ЕСТЬ двоеточие в данном случае означает где находится String
 
   // если String добавляется в начало другой String, используется ++:
   // если String добавляется в конец другой String, используется :++
 
   // операторы ++ и + работают со String и присоединяют все это (разница в производительности)
+
 
   //========================интерполяция строк (подстановка переменных)===============================
   //интерполяция применяется когда в строку необходимо подставить какое-то значение переменной
--- src/playground/ScalaPlayground.scala
+++ src/playground/ScalaPlayground.scala
@@ -1,27 +1,22 @@
 package playground
-
+import scala.annotation.tailrec
 object ScalaPlayground extends App{
-  import scala.annotation.tailrec
+  class MyString(val str: String) {
+    private var extra = "extraData"
 
-  def sumAndPrintInt(x : Int, y : Int, n : Int) : Unit = {
-    @tailrec
-    // функция для добавления чисел
-    def loop_sum(x : Int = x, y : Int = y, n : Int = n) : Int = {
-      if(n < 1) x
-      else loop_sum(x = x + y, n = n - 1)
-    }
-
-    @tailrec
-    // функция по подстчету количества разрядов
-    // здесь НЕТ ЦЕЛОЧИСЛЕННОГО деления
-    // а при делении Int на Int ВСЕГДА будет INT
-    def loop_count(n: Int, sign: Int = 10, count : Int = 1): Int = {
-      if ((n / sign) == 0) count
-      else loop_count(n, sign * 10, count + 1)
-    }
-
-    println(s"${loop_sum()} " * loop_count(loop_sum()) ++ "is the result")
+    def getString: String = str + extra
   }
 
-  sumAndPrintInt(10, 1, 5)
+  object MyString {
+    def apply(base: String, extras: String): MyString = {
+      val s = new MyString(base)
+      s.extra = extras
+      s
+    }
+
+    def apply(base: String) = new MyString(base)
+  }
+
+  println(MyString.apply("hello", "world").getString)
+  println(MyString.apply("welcome").getString)
 }
