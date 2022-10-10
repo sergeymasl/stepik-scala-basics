55a3e4106f470515754051604132bfd0bbc5db25
sergeymasl <serezha_maslov@mail.ru>
2022-10-10 18:02:32 +0300

    2022-10-10

--- src/lectures/week2oop/Generics.scala
+++ src/lectures/week2oop/Generics.scala
@@ -18,6 +18,42 @@ object Generics extends App {
   class SomeClass[B, C] {
     def someFunc(fEl : B, sEl : C) : Unit = println(s"first is ${fEl.getClass.getSimpleName}, second is ${sEl.getClass.getSimpleName}")
   }
-  val aVar = new SomeClass[String, Int]
-  aVar.someFunc("String", 2)
+  val aVar = new SomeClass[Int, String]
+  aVar.someFunc(2, "2")
+
+  // =========================================ограничения типов при обощении===========================
+  // разбирать будем на иерархии классов, следующей ниже:
+  //                                              Thing
+  //                                                |
+  //                                             Vehicle
+  //                                        |------------------|
+  //                                      Car               Bicycle
+  //                        |-------------|---------|             |
+  //                 Ambulance          Taxi      Jeep        Tricycle
+
+  trait Thing
+  class Vehicle extends Thing
+  class Car extends Vehicle
+  class Ambulance extends Car
+  class Taxi extends Car
+  class Jeep extends Car
+  class Bicycle extends Vehicle
+  class Tricycle extends Bicycle
+
+  // далее пропишем класс Parking
+  //class Parking[T](val vehicle: T)
+
+  // при объявлениии обобщенного класса в качестве обощения, помимо класса передаваемого параметра
+  // можно передать и его родительский(или выше по цепочке) класс
+  // например можно в качестве параметра передать класс Taxi, а в качестве обощения передать класс Taxi
+  val testParentClass = new Parking[Car](new Taxi)
+  println("The line above works")
+
+
+  // для того чтобы ограничить возможные обобщения существует ограничение типов
+  // верхнее ограничение <:
+  // нижнее ограничение >:
+
+  class Parking[T >: Bicycle <: Vehicle](val vehicle: T)
+  new Parking[Thing]()
 }
