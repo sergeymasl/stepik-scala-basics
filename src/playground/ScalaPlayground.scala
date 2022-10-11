7d867f43c9eaed2728edc09457989ef1459e8c28
sergeymasl <serezha_maslov@mail.ru>
2022-10-11 18:00:33 +0300

    2022-10-11

--- src/playground/ScalaPlayground.scala
+++ src/playground/ScalaPlayground.scala
@@ -1,5 +1,37 @@
 package playground
 import scala.annotation.tailrec
-object ScalaPlayground extends App{
-println(Int.MaxValue + 1)
+object ScalaPlayground extends App {
+  // дополните код
+  trait Event {
+    def id: String
+    def evType: String
+    def operation: String
+  }
+
+  case class UserEvent(id: String, evType: String, operation: String) extends Event
+  case class SystemEvent(id: String, evType: String, operation: String) extends Event
+
+  trait Registrable[A <: Event] {
+    def update(event : A, status: String) : A
+  }
+
+  object EventOps {
+    val userEvent = new Registrable[UserEvent] {
+      def update(event: UserEvent, status: String): UserEvent = event.copy(operation = status)
+    }
+
+    val systemEvent = new Registrable[SystemEvent] {
+      def update(event: SystemEvent, status: String): SystemEvent = event.copy(operation = status)
+    }
+  }
+
+  class EventOps[B <: Event](event : B)(ops: Registrable[B]) {
+    def update(status: String) = ops.update(event, status)
+  }
+
+  val user1Event = UserEvent("1", "user_event", "account_create")
+  val user1Ops = new EventOps(user1Event)(EventOps.userEvent)
+  val user1EventUpd = user1Ops.update("permission_add")
+
+  println(s"User1 | Operation: ${user1EventUpd.operation}")
 }
